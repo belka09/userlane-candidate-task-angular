835a26a92f207626c9cb188ed1fb6f259e433d57
Dmitry Zimnitsky <dmitryft4@gmail.com>
2024-10-22 10:32:33 +0200

    improve UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
@@ -7,12 +7,14 @@ import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { selectLoading } from './core/store/users/users.selector';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     RouterOutlet,
@@ -25,7 +27,10 @@ export class AppComponent {
   title = 'Userlane Angular Task';
   loading$: Observable<boolean>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private cdr: ChangeDetectorRef) {
     this.loading$ = this.store.pipe(select(selectLoading));
+    this.loading$.subscribe(() => {
+      this.cdr.markForCheck();
+    });
   }
 }
